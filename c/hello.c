9b9afb6898993d2d0e4632feec2a48ed488d2bfd
noahmcge <corphos111@gmail.com>
2019-02-04 10:06:42 -0800

    aa

---
+++ c/hello.c
@@ -0,0 +1,21 @@
+// hello.c
+//compile gcc hello.c -o hello.o
+// run ./hello.o
+
+#include <stdio.h>
+
+int main(){
+	printf("Hello, world! OHHHH YEAA!!/n");
+	printf("uwu /n");
+	printf("( ͡° ͜ʖ ͡°) /n");
+	printf("******************************************");
+	printf("*[̲̅$̲̅(̲̅5̲̅)̲̅$̲̅][̲̅$̲̅(̲̅5̲̅)̲̅$̲̅][̲̅$̲̅(̲̅5̲̅)̲̅$̲̅]*/n");
+	printf("******************************************");
+	printf("Noah McGehee");
+	printf("Noa cGehee");
+	printf("No Gehee");
+	printf("N ehee");
+	printf("hee");
+	return 0;
+	
+}
