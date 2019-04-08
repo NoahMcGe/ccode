8d9698a985c76ca752f2088ae7b320f137a41737
root <root@milkman.ksd.ord>
2019-04-08 10:14:52 -0700

    aa

--- sincos0.cpp
+++ sincos0.cpp
@@ -2,15 +2,22 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+double round4(double var)
+{
+	double value = (int)(var * 10000 + 0.0005);
+	return (double)value / 10000;
+}
 int main (){
 double PI=3.14159265359;
-double rad = 0;
-double t, result=0;// theta
+double rad, t, sr, cr;
 	for ( t=0 ; t<=360 ; t = t + 15 ) 
 	{
 	  rad = t * (PI / 180);
-	  result = sin(rad);
-	    cout <<t<<"\t"<< result<<endl;
+	  sr = sin(rad);
+	  sr = round4(sr);
+	  cr = cos(rad);
+	  cr = round4(cr);
+	    cout <<t<<"\t"<<cr<<"\t"<<sr<<"\n";
     }
     return 0;
 }
