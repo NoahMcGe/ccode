8372be0047bcdbac068a69bb31195a0fe01db888
noah <corphos111@gmail.com>
2019-05-14 09:49:30 -0700

    aa

---
+++ pointers/pointers3.cpp
@@ -0,0 +1,36 @@
+// nm pointers3.cpp
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int integers[10];
+	int * pointer;
+	pointer = integers;
+	
+	cout<<"print integers[] array"<<"\n\n";
+	for (int n = 0; n < 5; n++){
+		
+		cout << integers[n]<<"\n";
+	}
+	
+	*pointer = 11;
+	pointer++; *pointer = 22;
+	pointer++; *pointer = 33;
+	pointer++; *pointer = 44;
+	pointer++; *pointer = 55;
+	
+	//print array memory
+	cout<<"\nprint &integers[] memory address "<<"\n\n";
+	for (int n = 0; n < 5; n++){
+		cout << &integers[n]<<"\n";
+		cout<<endl;
+	}
+	cout<<"\nprint &integers[] memory address "<<"\n\n";
+	for (int n = 0; n < 5; n++){
+		cout << integers[n]<<"\n";
+		cout<<endl;
+	}
+	
+	return 0;
+}
